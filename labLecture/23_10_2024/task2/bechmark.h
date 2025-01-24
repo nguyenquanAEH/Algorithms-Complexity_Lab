fb28801aff266a616d7724abc2c042c332b3d158
Nguyen Anh Quan <nguyeq7411730_aehit@students.vizja.pl>
2025-01-24 21:16:36 +0700

    Create bechmark.h

---
+++ labLecture/23_10_2024/task2/bechmark.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <chrono>
+template<typename D = std::chrono::microseconds>
+class Benchmark
+{
+public:
+	Benchmark()
+	{
+		start = std::chrono::high_resolution_clock::now();
+	}
+	typename D::rep elapsed() const
+	{
+		auto end = std::chrono::high_resolution_clock::now();
+		auto result = std::chrono::duration_cast<D>(end - start);
+		return result.count();
+	}
+private:
+	std::chrono::high_resolution_clock::time_point start;
+};
